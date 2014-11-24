32dede19303dcaac18ed1d041e0c5006a2346096
Anders Janmyr <anders.janmyr@sonymobile.com>
2014-11-24 13:17:02 +0100

    Initial

---
+++ install-lambda.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+bin=${BASH_SOURCE[0]%/*.sh}
+
+program=`basename $0`
+
+set -o errexit
+
+function usage() {
+  echo "Usage: $program <function.js>"
+  echo "  --dry - don't to anything."
+}
+
+if [ $# -lt 1 ]
+then
+  echo 'Missing required parameters'
+  usage
+  exit 1
+fi
+
+main=${1}
+file="./${main}.js"
+zip="./${main}.zip"
+
+zip_package() {
+  zip -r $zip $file lib node_modules
+}
+
+upload_package() {
+  aws lambda upload-function \
+     --region eu-west-1 \
+     --function-name $main  \
+     --function-zip $zip \
+     --role arn:aws:iam::356671443308:role/executionrole  \
+     --mode event \
+     --handler $main \
+     --runtime nodejs \
+     --debug \
+     --profile adminuser \
+     --timeout 10 \
+     --memory-size 1024
+}
+
+
+# main
+zip_package
+upload_package
+
