3b81cf4c824eb6d412f7db5bab15bc230d4167c4
Anders Janmyr <anders@janmyr.com>
2014-12-07 17:01:20 +0100

    Extraced role into variable

--- install-lambda.sh
+++ install-lambda.sh
@@ -20,17 +20,19 @@ fi
 main=${1%.js}
 file="./${main}.js"
 zip="./${main}.zip"
-
+role='arn:aws:iam::638281126589:role/lambda_exec_role'
 zip_package() {
   zip -r $zip $file lib node_modules
 }
+
+
 
 upload_package() {
   aws lambda upload-function \
      --region eu-west-1 \
      --function-name $main  \
      --function-zip $zip \
-     --role arn:aws:iam::638281126589:role/lambda_exec_role \
+     --role $role\
      --mode event \
      --handler $main.handler \
      --runtime nodejs \
